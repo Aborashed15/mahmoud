bd4341032dea5e61eff73963151e0fba0aa9900c
Aborased15 <ahhdhdhdhdhhsid@gmail.com>
2021-10-19 08:46:09 -0700

    first commit

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, sum;
+	cout << "enter 2 number" << endl;
+	cin >> num1 >> num2;
+	sum = num1 + num2
+		cout << sum;
+    return 0;
+}
+
---
+++ Source.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num1, num2, sum;
+	cout << "enter 2 number" << endl;
+	cin >> num1 >> num2;
+	sum = num1 + num2;
+		cout << sum;
+		sytem("pause");
+	return 0;
+}
+
